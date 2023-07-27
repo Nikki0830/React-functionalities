07b7dd648c9d8bd52aca46df94779935a91fa24d
Nikki0830 <gniki0830@gmail.com>
2023-07-27 12:34:29 +0530

    COVID19-TRACKER

--- react-functionality/src/App.js
+++ react-functionality/src/App.js
@@ -4,7 +4,7 @@ import Covid from './Component/LiveCovidTracker/Covid';
 
 function App() {
   return (
-    <div className="App">
+    <div>
     <Covid/>
     </div>
   );
--- react-functionality/src/Component/LiveCovidTracker/Covid.js
+++ react-functionality/src/Component/LiveCovidTracker/Covid.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
-function Covid() {
+const Covid = () => {
+  const getCovidData = async () => {
+    try {
+      const res = await fetch("https://data.covid19india.org/data.json");
+      //   console.log(res);
+      const actualData = await res.json();
+      console.log(actualData.statewise[0]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    // getCovidData();
+  }, []);
   return (
-    <div>
-      <h1>Covid Tracker</h1>
-    </div>
-  )
-}
+    <>
+      <section>
+        <h1> &#128721; LIVE</h1>
+        <h1>COVID-19 CORONAVIRUS TRACKER</h1>
+        <ul>
+          <li className="card">
+            <div className="card-main">
+              <div className="card-inner">
+                <p className="card-name">
+                  <span>OUR</span> COUNTRY
+                </p>
+                <p className="card-total card-small">INDIA</p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </section>
+    </>
+  );
+};
 
-export default Covid
+export default Covid;
